d3a7d2f66bac947e63fbc361e8d76d9af50de9dc
tiurmagrace <tiursihaloho210905@gmail.com>
2025-03-11 11:39:23 +0700

    menambahkan class HaloGeneric dengan method

---
+++ DataGeneric.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace tpmodul5_2311104042
+{
+    public class DataGeneric<T>
+    {
+        private T data;
+
+        public DataGeneric(T data)
+        {
+            this.data = data;
+        }
+
+        public void PrintData()
+        {
+            Console.WriteLine($"Data yang tersimpan adalah: {data}");
+        }
+    }
+}
